23950e11ed51756dedf72347fea31b47f6c97c7a
duanniten <duann_@hotmail.com>
2024-09-24 09:04:01 -0300

    change

--- scourgify.py
+++ scourgify.py
@@ -15,8 +15,23 @@ elif len(sys.argv[2]) < 3:
 elif sys.argv[2][-4:] != '.csv':
     sys.exit('Not a CSV file')
 
+students = []
 try:
-    with open(sys.argv[1]) as file:
-        reader = 
+    with open(sys.argv[1], 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            splitted_name = row['name'].split(sep=', ')
+            first = splitted_name[1]
+            last = splitted_name[0]
+            students.append({
+                'first': first,
+                'last' : last,
+                'house': row['house']
+            })
+
 except FileNotFoundError:
     sys.exit(f'Could not read {sys.argv[1]}')
+
+with open(sys.argv[2], 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=['first', 'last', 'house'])
+    writer.writerows(students)
